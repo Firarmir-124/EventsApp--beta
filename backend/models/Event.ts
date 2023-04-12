7deb85143f2f1e4a16bb0abd715b053910ef5f9d
Polushin Dima <dima2004polushin2004@gmail.com>
2023-04-12 13:26:49 +0600

    #backend Создал схему Event

---
+++ backend/models/Event.ts
@@ -0,0 +1,37 @@
+import { model, Schema } from 'mongoose';
+import { EventType } from '../types';
+
+const EventSchema = new Schema<EventType>({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  speaker: {
+    type: [{ name: String }],
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  hashtag: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+const Event = model('Event', EventSchema);
+export default Event;
--- backend/types.d.ts
+++ backend/types.d.ts
@@ -1,7 +1,23 @@
+import { Types } from 'mongoose';
+
 export interface IUser {
   email: string;
   password: string;
   token: string;
   role: string;
   displayName: string;
+}
+
+export interface EventType {
+  title: string;
+  description: string;
+  speaker: [
+    {
+      name: string;
+    },
+  ];
+  time: string;
+  image: string;
+  hashtag: string;
+  user: Types.ObjectId;
 }
