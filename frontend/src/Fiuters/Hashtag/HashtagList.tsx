4260d87a89fd885ee1c0d7878787022af419ae78
Polushin Dima <dima2004polushin2004@gmail.com>
2023-04-15 15:18:20 +0600

    #frontend Написал логику удаления хэштега

--- frontend/src/Fiuters/Hashtag/HashtagList.tsx
+++ frontend/src/Fiuters/Hashtag/HashtagList.tsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react';
-import { Alert, CircularProgress } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Alert, CircularProgress, Snackbar } from '@mui/material';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { selectHashtagList, selectHashtagListLoading } from './hashtagSlice';
-import { fetchHashtagList } from './hashtagThunk';
+import { deleteHashtag, fetchHashtagList } from './hashtagThunk';
 import CardHashtag from './components/CardHashtag/CardHashtag';
 
 const HashtagList = () => {
+  const [open, setOpen] = useState(false);
   const dispatch = useAppDispatch();
   const listHashtag = useAppSelector(selectHashtagList);
   const loadingListHashtag = useAppSelector(selectHashtagListLoading);
@@ -14,17 +15,31 @@ const HashtagList = () => {
     dispatch(fetchHashtagList());
   }, [dispatch]);
 
+  const removeHashtagCard = async (id: string) => {
+    await dispatch(deleteHashtag(id)).unwrap();
+    await dispatch(fetchHashtagList()).unwrap();
+    setOpen(true);
+  };
+
   return (
     <>
       {!loadingListHashtag ? (
         listHashtag.length !== 0 ? (
-          listHashtag.map((hashtag) => <CardHashtag key={hashtag._id} hashtag={hashtag} />)
+          listHashtag.map((hashtag) => (
+            <CardHashtag removeHashtagCard={() => removeHashtagCard(hashtag._id)} key={hashtag._id} hashtag={hashtag} />
+          ))
         ) : (
           <Alert severity="info">В данный момент хэштегов нет</Alert>
         )
       ) : (
         <CircularProgress />
       )}
+
+      <Snackbar open={open} autoHideDuration={6000} onClose={() => setOpen(false)}>
+        <Alert onClose={() => setOpen(false)} severity="success" sx={{ width: '100%' }}>
+          Хэштег успешно удалён !
+        </Alert>
+      </Snackbar>
     </>
   );
 };
--- frontend/src/Fiuters/Hashtag/components/CardHashtag/CardHashtag.tsx
+++ frontend/src/Fiuters/Hashtag/components/CardHashtag/CardHashtag.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { ButtonGroup, Divider, IconButton, List, ListItemIcon, ListItemText } from '@mui/material';
+import { ButtonGroup, CircularProgress, Divider, IconButton, List, ListItemIcon, ListItemText } from '@mui/material';
 import InboxIcon from '@mui/icons-material/Inbox';
 import { HashtagListType } from '../../../../types';
 import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
 import EditIcon from '@mui/icons-material/Edit';
 import { useAppSelector } from '../../../../app/hooks';
 import { selectUser } from '../../../User/usersSlice';
+import { selectRemoveLoading } from '../../../Event/eventSlice';
 
 interface Props {
   hashtag: HashtagListType;
+  removeHashtagCard: React.MouseEventHandler;
 }
 
-const CardHashtag: React.FC<Props> = ({ hashtag }) => {
+const CardHashtag: React.FC<Props> = ({ hashtag, removeHashtagCard }) => {
   const user = useAppSelector(selectUser);
+  const removeLoading = useAppSelector(selectRemoveLoading);
   return (
     <>
       <List sx={{ display: 'flex', alignItem: 'center' }}>
@@ -23,8 +26,8 @@ const CardHashtag: React.FC<Props> = ({ hashtag }) => {
 
         {user && user.role === 'organizer' ? (
           <ButtonGroup disableElevation variant="contained" aria-label="Disabled elevation buttons">
-            <IconButton aria-label="delete">
-              <RemoveCircleIcon />
+            <IconButton disabled={removeLoading} onClick={removeHashtagCard} aria-label="delete">
+              {!removeLoading ? <RemoveCircleIcon /> : <CircularProgress />}
             </IconButton>
             <IconButton aria-label="edit">
               <EditIcon />
