9606ae8d5675c8f10a292055f209fa3e740b53e4
Polushin Dima <dima2004polushin2004@gmail.com>
2023-04-30 18:27:23 +0600

    #frontend изменил верстку компонента CardEventUser

--- frontend/src/features/Event/components/CardEventUser.tsx
+++ frontend/src/features/Event/components/CardEventUser.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Card, CardContent, CardMedia, Grid, Typography } from '@mui/material';
+import { Card, CardContent, CardMedia, Chip, Grid, IconButton, Paper, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { EventList } from '../../../types';
 import { apiURL } from '../../../constants';
 import eventImage from '../../../assests/images/event.png';
 import dayjs from 'dayjs';
 import { linksStyle } from '../../../components/Layout/Layout';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 interface Props {
   event: EventList;
@@ -20,7 +21,31 @@ const CardEventUser: React.FC<Props> = ({ event }) => {
 
   return (
     <Grid item xs={2} sm={4} md={4}>
-      <Card>
+      <Card sx={{ position: 'relative' }}>
+        <Paper
+          sx={{
+            background: 'rgb(0 0 0 / 54%)',
+            position: 'absolute',
+            width: '100%',
+            height: '50px',
+          }}
+        >
+          <Grid container display="flex" justifyContent="space-between" alignItems="center" padding="5px">
+            <Grid item>
+              <Chip
+                sx={{ background: '#fff', color: '#000' }}
+                size="small"
+                label={event.hashtag.name}
+                color="primary"
+              />
+            </Grid>
+            <Grid item>
+              <IconButton>
+                <FavoriteIcon sx={{ color: '#fff' }} />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Paper>
         <Link to={'/full_event/' + event._id}>
           <CardMedia sx={{ height: 300 }} image={image} title="green iguana" />
         </Link>
