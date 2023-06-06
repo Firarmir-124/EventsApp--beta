24d4d93e3db9bb1db3a230c7d47800b9efa2668d
Polushin Dima <dima2004polushin2004@gmail.com>
2023-06-06 19:08:06 +0600

    #frontend изменил верстку в компонентах избранного

--- frontend/src/features/Profile/Favorites/Favorites.tsx
+++ frontend/src/features/Profile/Favorites/Favorites.tsx
@@ -1,22 +1,26 @@
 import React, { useEffect } from 'react';
-import { Alert, Avatar, Box, Button, CircularProgress, Container, Grid, Typography } from '@mui/material';
+import { Alert, Avatar, Box, Button, Container, Grid, Typography } from '@mui/material';
 import CardFavorite from '../components/CardFavorite';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import BallotIcon from '@mui/icons-material/Ballot';
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
 import { selectFavoritesFetchLoading, selectFavoritesList } from '../profileSlice';
 import { fetchFavorites, removeFavorites, showFavorites } from '../profileThunk';
+import useConfirm from '../../../components/Dialogs/Confirm/useConfirm';
+import SkeletonCardUser from '../../Event/components/SkeletonCardUser';
+
 const Favorites = () => {
   const dispatch = useAppDispatch();
   const favoritesList = useAppSelector(selectFavoritesList);
   const loadingFavoritesList = useAppSelector(selectFavoritesFetchLoading);
+  const { confirm } = useConfirm();
 
   useEffect(() => {
     dispatch(fetchFavorites());
   }, [dispatch]);
 
   const removeOneCard = async (id: string) => {
-    if (window.confirm('Вы действительно хотите удалить ?')) {
+    if (await confirm('Уведомление', 'Вы действительно хотите удалить ?')) {
       await dispatch(removeFavorites({ deleteOne: id, deleteAll: undefined, deleteSelect: undefined })).unwrap();
       await dispatch(fetchFavorites());
     } else {
@@ -30,7 +34,7 @@ const Favorites = () => {
   };
 
   const removeSelected = async () => {
-    if (window.confirm('Вы действительно хотите удалить ?')) {
+    if (await confirm('Уведомление', 'Вы действительно хотите удалить ?')) {
       await dispatch(removeFavorites({ deleteOne: undefined, deleteAll: undefined, deleteSelect: true })).unwrap();
       await dispatch(fetchFavorites());
     } else {
@@ -60,24 +64,22 @@ const Favorites = () => {
           Избранное
         </Typography>
         <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-          {favoritesList ? (
-            !loadingFavoritesList ? (
-              favoritesList.event.length > 0 ? (
-                favoritesList.event.map((event) => (
-                  <CardFavorite
-                    key={event._id}
-                    event={event}
-                    removeOneCard={() => removeOneCard(event.list._id)}
-                    showFavoritesCard={() => showFavoritesCard(event.list._id)}
-                  />
-                ))
+          {favoritesList && favoritesList.event.length > 0 ? (
+            favoritesList.event.map((event) =>
+              !loadingFavoritesList ? (
+                <CardFavorite
+                  key={event._id}
+                  event={event}
+                  removeOneCard={() => removeOneCard(event.list._id)}
+                  showFavoritesCard={() => showFavoritesCard(event.list._id)}
+                />
               ) : (
-                <Alert severity="info">Список пуст</Alert>
-              )
-            ) : (
-              <CircularProgress />
+                <SkeletonCardUser key={event._id} />
+              ),
             )
-          ) : null}
+          ) : (
+            <Alert severity="info">В данный момент список пуст</Alert>
+          )}
         </Grid>
       </Box>
     </Container>
